8d0f811ff6ff3fbeb0cf8f383af6a66ac5c77322
tegorov <taras.yegorov@indriver.com>
2022-01-23 15:35:13 +0300

    Add properties

---
+++ src/main/java/ru/tegorov/AllureEnvironmentGenerateMojo.java
@@ -0,0 +1,64 @@
+package ru.tegorov;
+
+/*
+ * Copyright 2001-2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+@Mojo(name = "allure-environment", defaultPhase = LifecyclePhase.VALIDATE)
+public class AllureEnvironmentGenerateMojo extends AbstractMojo {
+
+    private static final String ALLURE_ENVIRONMENT_PROPERTIES = "environment.properties";
+
+    @Parameter(required = true, readonly = true)
+    protected Properties properties;
+
+    @Parameter(defaultValue = "target/allure-results", readonly = true)
+    protected String allureResultsDirectory;
+
+    public void execute() throws MojoExecutionException {
+        getLog().info("Generate Allure Environment to " + allureResultsDirectory);
+
+        if (properties.size() == 0) {
+            getLog().warn("Allure Environment was skipped because properties is empty");
+            return;
+        }
+
+        File allureResultsDir = new File(allureResultsDirectory);
+        allureResultsDir.mkdirs();
+
+        File propertiesFile = new File(allureResultsDirectory, ALLURE_ENVIRONMENT_PROPERTIES);
+        if (propertiesFile.exists()) {
+            propertiesFile.delete();
+        }
+
+        try (FileOutputStream outputStrem = new FileOutputStream(propertiesFile)) {;
+            properties.store(outputStrem, null);
+            getLog().info("Allure Environment generated successfully");
+        } catch (IOException e) {
+            getLog().error("Generation error", e);
+        }
+    }
+}
